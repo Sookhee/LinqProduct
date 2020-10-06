cea585343c09e8ee55626f4f891754ab8e904aad
Jeong Minji <s2018s38@e-mirim.hs.kr>
2020-10-06 09:52:45 +0900

    초기 커밋.

---
+++ LinqProduct/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProduct
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return Name + " : " + Price + "원";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> input = new List<Product>()
+            {
+                new Product(){Name="고구마", Price=1500},
+                new Product(){Name="사과", Price=2000},
+                new Product(){Name="배", Price=2500},
+                new Product(){Name="양배추", Price=3000},
+                new Product(){Name="옥수수", Price=2000},
+                new Product(){Name="자두", Price=500}
+            };
+        }
+    }
+}
